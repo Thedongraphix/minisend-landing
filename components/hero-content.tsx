44cdda2544ede0c5de94337609afdc66c89e4a41
Thedongraphix <chrisoketchcn@gmail.com>
2025-11-21 15:42:47 +0300

    New hero section

--- components/hero-content.tsx
+++ components/hero-content.tsx
@@ -2,7 +2,7 @@
 
 export default function HeroContent() {
   return (
-    <main className="absolute top-32 left-4 right-4 sm:top-28 sm:left-8 sm:right-auto z-20 max-w-full sm:max-w-3xl px-2">
+    <main className="absolute top-40 left-4 right-4 sm:top-28 sm:left-8 sm:right-auto z-20 max-w-full sm:max-w-3xl px-2">
       <div className="text-left">
         <div
           className="inline-flex items-center px-3 py-1 rounded-full bg-white/5 backdrop-blur-sm mb-5 sm:mb-6 relative"
@@ -16,15 +16,14 @@ export default function HeroContent() {
 
         {/* Main Heading */}
         <h1 className="text-5xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl tracking-tight font-light text-white mb-6 sm:mb-7 leading-[1.1]">
-          <span className="font-medium italic instrument">Offramp</span> USDC in
+          <span className="font-light tracking-tight text-white">Onchain earnings truly</span>
           <br />
-          <span className="font-light tracking-tight text-white">Less than 60 Seconds</span>
+          <span className="font-medium italic instrument">spendable</span> <span className="font-light tracking-tight text-white">for creators</span>
         </h1>
 
         {/* Description */}
         <p className="text-base sm:text-base md:text-lg font-light text-white/70 mb-8 sm:mb-8 leading-relaxed max-w-lg sm:max-w-2xl">
-          Convert your USDC earnings to Nigerian Naira or Kenyan Shillings instantly. Built for creators on Farcaster and
-          BaseApp.
+          Convert your USDC earnings to local currency in seconds
         </p>
 
         {/* Buttons */}
