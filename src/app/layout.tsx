5b91dad257593f35e00bf3119764877bf681353b
Thedongraphix <chrisoketchcn@gmail.com>
2026-01-29 21:26:24 +0300

    Adding google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import Layout from '@/components/Layout';
 import { StructuredData, FAQStructuredData } from '@/components';
+import Script from 'next/script';
 import './globals.css';
 import type { Metadata } from 'next';
 
@@ -165,6 +166,20 @@ export default function RootLayout({
 
       </head>
       <body>
+        {/* Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-44WPGTWH8S"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-44WPGTWH8S');
+          `}
+        </Script>
+
         <StructuredData />
         <FAQStructuredData />
         <Layout>{children}</Layout>
